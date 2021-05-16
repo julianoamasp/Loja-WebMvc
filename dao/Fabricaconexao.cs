5ce411a43a812fb1a5165eac5769f56d626f68c7
Juliano da Silva Mendes <julianoamasp@gmail.com>
2021-05-15 23:52:33 -0300

    Separando a conexão.

---
+++ dao/Fabricaconexao.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Loja.dao
+{
+    public static class Fabricaconexao
+    {
+        public static MySqlConnection conexao()
+        {
+            MySqlConnection conn = new MySqlConnection("Server=localhost;User ID=root;Password=;Database=loja");
+            return conn;
+        }
+       
+    }
+}
